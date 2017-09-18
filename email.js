ac2ff64559840e839fd441c1fee0a2a1b050d8a1
Maria Rymer <mariarymer@Marias-MacBook-Pro.local>
2017-09-18 12:16:10 -0400

    First commit

---
+++ email.js
@@ -0,0 +1,37 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+nodemailer.createTestAccount((err, account) => {
+  if (err) {
+    console.error('Failed to create a testing account.' + err.message);
+    return process.exit(1)
+  }
+
+  let transporter= nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: fals,
+    auth: {
+      user: account.user,
+      pass: account.pass,
+    }
+
+  });
+
+  let mailOptions ={
+    from: '"Maria Rymer" <mariaj.rymer@gmail.com>', //sender address
+    to: '<rymer.mariaj@gmail.com>', //receiver
+    subject: 'Hello', //subject line
+    text: 'Hey, what up?', //plain text
+    html: '<b> Hey, what up?</b>' //hmtl
+  };
+
+  transporter.sendMail(mailOptions,(error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  });
+});
